03d779f79f4832d72dc83e0a50eb24c1fb22fa48
NDAGIJIMANA Bernard <ndgjmnbernard@gmail.com>
2025-09-26 16:56:17 +0200

    Add files via upload

---
+++ assignment_17_sept_2025/StudentGradingSystem.java
@@ -0,0 +1,13 @@
+package assignment_17_sept_2025;
+
+public class StudentGradingSystem {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ assignment_17_sept_2025/Supemarket.java
@@ -0,0 +1,13 @@
+package assignment_17_sept_2025;
+
+public class Supemarket {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
